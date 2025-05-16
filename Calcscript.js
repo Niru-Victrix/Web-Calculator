0fdb8492327421a848b1822e5fc4f8007a1bfaa1
Jhonil Garcia <jhonil.garcia016@gmail.com>
2025-05-16 16:30:43 +0800

    fully functioning

--- Calcscript.js
+++ Calcscript.js
@@ -10,7 +10,19 @@ arr.forEach(button => {
             input.value = string;
         }
         
-        string += e.target.innerHTML;
-        input.value = string;
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+        
     })
 })
